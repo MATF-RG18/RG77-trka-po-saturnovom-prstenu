7ac70b425e0148bdc558aa7c1b5dcb3415b094b2
mi15110 <kpetrovic134@gmail.com>
2018-12-16 23:59:08 +0100

    Kreiranje Saturnovog prstena, staza po prstenu i pozicioniranje scene.

--- game.c
+++ game.c
@@ -1,10 +1,18 @@
 #include <stdlib.h>
+#include <math.h>
 #include <GL/glut.h>
+
+#define PI 3.14159265359
 
 void on_reshape(int width, int height);
 void on_display(void);
 void on_keyboard(unsigned char key, int x, int y);
-void drawAxes();
+
+void draw_axes();
+void draw_ring(float in_radius, float out_radius, int precision);
+
+void draw_Saturn_ring();
+void draw_Saturn_sphere();
 
 static int rotation_angle = 0;
 
@@ -12,10 +20,10 @@ int main(int argc, char** argv) {
     
     /* Inicijalizacija GLUT-a. */
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     
     /* Kreiranje prozora. */
-    glutInitWindowSize(800, 500);
+    glutInitWindowSize(1000, 600);
     glutInitWindowPosition(100, 100);
     glutCreateWindow(argv[0]);
     
@@ -26,7 +34,7 @@ int main(int argc, char** argv) {
     
     /* OpenGL Inicijalizacija. */
     glClearColor(0, 0, 0, 0);
-    
+    glEnable(GL_DEPTH_TEST);
     /* Ulazi se u glavnu petlju. */
     glutMainLoop();
     
@@ -34,7 +42,7 @@ int main(int argc, char** argv) {
 }
 
 /* Pomoćna funkcija za iscrtavanje koordinatnih osa. */
-void drawAxes()
+void draw_axes()
 {
 glBegin( GL_LINES );
 glColor3f( 1.0, 0.0, 0.0);
@@ -59,43 +67,50 @@ glEnd();
 void on_display(void) {
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    drawAxes();
-    
+
     glColor3f(1,1,1);
     
-    
-    
     glPushMatrix();
-        glRotatef (rotation_angle, 0, 1.0, 0);
-        glRotatef(90, 1,0,0);
-        glutWireSphere(0.7,50,50);
+        glScalef(1.5,1.5,1.5);
+        glTranslatef(-0.70,-0.2,0.33);
+        draw_Saturn_ring();
+        draw_Saturn_sphere();
     glPopMatrix();
-    
-    
-    glPushMatrix();
-        glColor3f(0.5,0.5,0.5);
-        glRotatef (rotation_angle, 0, 1.0, 0);
-        glRotatef(70, 1,0,0);
-        glutWireTorus(0.1,1,50,50);   
-    glPopMatrix();
-
     
     glutSwapBuffers();
 }
 
+/* Iscrtavanje prstena. */
+void draw_ring(float in_radius, float out_radius, int precision) {
+    glBegin(GL_QUAD_STRIP);
+    int i;
+    for( i = 0; i <= precision; ++i )
+    {
+        float angle;
+        /* Trazimo uglove za iscrtavanje tacaka [0, 2PI] */
+        angle=2 * PI * ((i * 1.0) / precision);
+        
+        /* Iscrtavaju se grupisani poligoni. [GL_QUAD_STRIP]*/
+        glVertex2f( in_radius * cos(angle), in_radius * sin(angle));
+        glVertex2f( out_radius * cos(angle), out_radius * sin(angle));
+    }
+    glEnd();
+}
 
 /* Obrada događaja on on_reshape. */
 void on_reshape(int width, int height)
 {
-    glViewport(0, 0, width, height);
+    glViewport(0,0, width, height);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, (float) width / height, 1, 10);
+    gluPerspective(40, (float) width / height, 1, 10);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt (0, 0, 2, 0, 0, 0.0, 0.0, 1.0, 0.0);
+    gluLookAt (0,0,3, 0, 0, 0.0, 0.0, 1.0, 0.0);
+        
+    
 }
 
 /* Obrada događaja on on_keyboard. */
@@ -109,4 +124,33 @@ void on_keyboard(unsigned char key, int x, int y) {
             glutPostRedisplay();
             break;
     }
+}
+
+void draw_Saturn_ring() {
+    glPushMatrix();
+        glScalef(1,1,1.5);
+        glRotatef(100, 1,0,0);
+        
+        int i=0;
+        int precision = 100;
+        int num_of_speedways = 5;
+    
+        float inner = 0.8, outer = 0.9;
+        float color_rgb = 0.5;
+    
+        for (i=0; i<num_of_speedways; i++) {
+            glColor3f(color_rgb + 0.1*i,color_rgb + 0.1*i, color_rgb + 0.1*i);
+            draw_ring(inner + 0.1*i, outer + 0.1*i, precision);
+        }
+    glPopMatrix();
+}
+
+void draw_Saturn_sphere() {
+     glPushMatrix();
+        glColor3f(106.0/255,42.0/255,14.0/255);
+        glRotatef (rotation_angle, 0, 1.0, 0);
+        glRotatef(90, 1,0,0);
+        glutSolidSphere(0.7,50,50);
+        
+    glPopMatrix();
 }
